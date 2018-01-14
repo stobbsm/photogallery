f28d80d57a8dd28d5d7dddba01bfccc2c1d30e89
Matthew Stobbs <matthew@sproutingcommunications.com>
2018-01-14 14:31:03 -0700

    Started work on local bootstrap with npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('node_modules/jquery/dist/jquery.js', 'public/js/jquery.js')
-    .js('node_modules/bootstrap/dist/js/bootstrap.js', 'public/js/bootstrap.js')
-    .js('resources/assets/js/app.js', 'public/js/app.js')
+mix.js('resources/assets/js/app.js', 'public/js/app.js')
    .sass('resources/assets/sass/app.scss', 'public/css');
